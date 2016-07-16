b689484df48633a6085bdcf52125acc18f02ea56
peterpetrekanics <peter.petrekanics@liferay.com>
2016-07-16 17:19:33 +0200

    Question 1 completed

--- src/javabasicstest/Java_basics_test_from_Liferay.java
+++ src/javabasicstest/Java_basics_test_from_Liferay.java
@@ -1,3 +1,4 @@
+package javabasicstest;
 
 public class Java_basics_test_from_Liferay {
 
@@ -5,8 +6,12 @@ public class Java_basics_test_from_Liferay {
 		// Java test created by Liferay TS Team:
 		// https://in.liferay.com/web/support/documents?p_p_id=20&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-1&p_p_col_count=1&_20_struts_action=%2Fdocument_library%2Fview_file_entry&_20_redirect=https%3A%2F%2Fin.liferay.com%2Fweb%2Fsupport%2Fdocuments%3Fp_p_id%3D20%26p_p_lifecycle%3D0%26p_p_state%3Dnormal%26p_p_mode%3Dview%26p_p_col_id%3Dcolumn-1%26p_p_col_count%3D1%26_20_action%3DbrowseFolder%26_20_struts_action%3D%252Fdocument_library%252Fview%26_20_folderId%3D1447328&_20_fileEntryId=1447342
 		
-		
 
+//		Question 1 : What will be the output of the program?
+//		Answer 1: 0 42 42
+		
+		
+		
 	}
 
 }
---
+++ src/question1/Two.java
@@ -0,0 +1,5 @@
+package question1;
+
+class Two {
+	byte x;
+}
